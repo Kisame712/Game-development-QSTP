c78c7b96d02d5fbe978210afefec2e9abb8e32de
Kisame712 <77425068+Kisame712@users.noreply.github.com>
2021-09-27 07:22:27 +0530

    Add files via upload
    These are the C# scripts used for this project.

---
+++ EnemyBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] float movespeed = 1f;
+    Rigidbody2D myRigidbody;
+   
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    { if (isfacingRight())
+            myRigidbody.velocity = new Vector2(movespeed,0f);
+        else
+            myRigidbody.velocity = new Vector2(-movespeed,0f);   
+    }
+    private bool isfacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), transform.localScale.y);
+    }
+}
